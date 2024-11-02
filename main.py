a7992d0f1a1424b057f655a07862a26f1913ef6c
MrCyclopede <romain.courtois.mails@gmail.com>
2024-11-02 21:22:27 +0100

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import ollama
 
 stream = ollama.chat(
     model='llama3.2:3b',
-    messages=[{'role': 'user', 'content': 'Why is the sky blue?'}],
+    messages=[{'role': 'user', 'content': 'Explain simply the main benefits of a cloud dev environment to try out new dev tools in your browser'}],
     stream=True,
 )
+
+print("llama3.2 response:")
 
 for chunk in stream:
   print(chunk['message']['content'], end='', flush=True)
